fc7eb56890f30f85bb6ab3d863f727385dbb45e0
enoma-coder <enomavictor2002@gmail.com>
2020-09-25 01:39:57 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,65 @@
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+
+const homeStartingContent =
+  "Lorem ipsum dolor sit amet, suas ponderum laboramus duo ad, his eu dicunt conclusionemque.Periculis interpretaris ex per, nominavi mediocritatem nec at.";
+
+const aboutContent =
+  " Officiis interesset sadipscing no vis. Elitr tritani erroribus per et. An quo esse simul petentium,";
+
+const contactContent =
+  " Erat tibique partiendo an eos, ne pri unum dicunt, unum fuisset ne vim.Melius lobortis consulatu eum ea, quo fugit corpora eu.";
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
+
+const posts = [];
+
+app.get("/", function (req, res) {
+  res.render("home", { startingContent: homeStartingContent, display: posts });
+});
+
+app.get("/about", function (req, res) {
+  res.render("about", { about: aboutContent });
+});
+
+app.get("/contact", function (req, res) {
+  res.render("contact", { contactContent: contactContent });
+});
+app.get("/compose", function (req, res) {
+  res.render("compose");
+});
+
+app.post("/compose", function (req, res) {
+  const composeBody = {
+    title: req.body.postTitle,
+    body: req.body.postBody,
+  };
+  posts.push(composeBody);
+
+  res.redirect("/");
+});
+app.get("/posts/:posted", function (req, res) {
+  const postTitle = _.lowerCase(req.params.posted);
+  for (i = 0; i < posts.length; i++) {
+    const composeLowercase = _.lowerCase(posts[i].title);
+
+    if (postTitle === composeLowercase) {
+      res.render("post", {
+        title: posts[i].title,
+        content: posts[i].body,
+      });
+    }
+  }
+});
+
+app.listen(process.env.PORT || 3030, function () {
+  console.log("server running at port 3030");
+});
