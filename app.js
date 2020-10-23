52c4a2ddf93ed7b22594a2df7783fb5cdd1e50cc
enoma-coder <enomavictor2002@gmail.com>
2020-10-23 20:37:02 +0200

    third commit

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
 const mongoose = require("mongoose");
+var domino = require("domino");
 
 const homeStartingContent =
   "Lorem ipsum dolor sit amet, suas ponderum laboramus duo ad, his eu dicunt conclusionemque.Periculis interpretaris ex per, nominavi mediocritatem nec at.";
@@ -24,35 +25,44 @@ app.use(express.static(__dirname + "/public"));
 mongoose.connect("mongodb://localhost:27017/blogDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useFindAndModify: false,
 });
 
 const blogSchema = new mongoose.Schema({
   title: String,
-  body: String,
+  content: String,
 });
 
 const Post = mongoose.model("post", blogSchema);
 
 const day1 = new Post({
-  title: "todays posts",
-  content: "whatsoever it is is what exactly it",
+  title: "day-1",
+  content: "go to the compose page to publish your blog",
 });
 const day2 = new Post({
-  title: "todays jaklhfuohaou",
-  content: "whatsoever it is is efklj exactly itbjalkbsvvvvvvvvvvjvjblamkm  hkjfvbjkhalsnpohsoihha[piohaosh ikoshvpiohia ijviaoh",
+  title: "day-2",
+  content: "click on each of the blog posts to delete a post",
 });
-// Post.updateOne({ "_id" : "5f9040b0be369d3640fe4bc1"}, {$set: {content: "whatsoever it is is efklj exactly itbjalkbsvvvvvvvvvvjvjblamkm  hkjfvbjkhalsnpohsoihha[piohaosh ikoshvpiohia ijviaoh"}}, (err) => err ? console.log(err): console.log("success"));
 
-const posts = ([day1, day2]);
+const posts = [day1, day2];
 
 app.get("/", function (req, res) {
-  Post.find({}, (err, posts) => {
-    err
-      ? console.log(err)
-      : res.render("home", {
-          startingContent: homeStartingContent,
-          display: posts
-        });
+  Post.find({}, (err, foundItems) => {
+    if (!foundItems) {
+      Post.insertMany(posts, (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("successfully inserted");
+        }
+      });
+      res.redirect("/");
+    } else {
+      res.render("home", {
+        startingContent: homeStartingContent,
+        display: foundItems,
+      });
+    }
   });
 });
 
@@ -72,22 +82,59 @@ app.post("/compose", function (req, res) {
     title: req.body.postTitle,
     content: req.body.postBody,
   };
-  posts.push(composeBody);
+
+  const newPost = new Post({
+    title: composeBody.title,
+    content: composeBody.content,
+  });
+
+  newPost.save();
 
   res.redirect("/");
 });
 app.get("/posts/:posted", function (req, res) {
-  const postTitle = _.lowerCase(req.params.posted);
-  for (i = 0; i < posts.length; i++) {
-    const composeLowercase = _.lowerCase(posts[i].title);
+  Post.find({}, (err, foundItems) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const postTitle = _.lowerCase(req.params.posted);
+      foundItems.forEach((post) => {
+        const composeLowercase = _.lowerCase(post.title);
 
-    if (postTitle === composeLowercase) {
-      res.render("post", {
-        title: posts[i].title,
-        content: posts[i].content,
+        if (postTitle === composeLowercase) {
+          res.render("post", {
+            title: post.title,
+            content: post.content,
+          });
+        }
       });
     }
-  }
+  });
+});
+
+var Element = domino.impl.Element; // etc
+
+var window = domino.createWindow("<h1>Hello world</h1>", "http://example.com");
+var document = window.document;
+
+var h1 = document.querySelector(".modal__btn ");
+console.log(h1);
+
+//delete post from both frontend and backend
+
+app.post("/delete", (req, res) => {
+  const deleteItem = req.body.deleteBtn;
+  const modalContent = document.getElementById("modal__content");
+  const modalBtn = document.querySelector(".modal__btn");
+  const btnSubmit = document.querySelector(".btn__submit");
+
+  btnSubmit.addEventListener("click", function () {
+    modalContent.body.style = "block";
+  });
+  Post.findByIdAndRemove(deleteItem, (err) => {
+    err ? console.log(err) : console.log("successfully deleted from database");
+  });
+  res.redirect("/");
 });
 
 app.listen(process.env.PORT || 3030, function () {
